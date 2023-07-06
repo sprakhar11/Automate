89257e6d0bc28063c8b9e7c7b2ab11de86cc14ca
sprakhar11 <prakhar.infinity@gmail.com>
2023-07-06 16:24:45 +0530

    index updated

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const puppeteer = require('puppeteer');
+require("dotenv").config();
 
 async function openAndClosePage(url) {
   while (true) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        agrs: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+        ],
+        executablePath:
+            process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),}
+    );
     const page = await browser.newPage();
 
     await page.goto(url);
